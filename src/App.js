cba82e9fc6aa6b5be8d362b96eed513492680d10
Ayush <ayushaggarwal9042@gmail.com>
2021-02-23 19:44:08 +0530

    highlight winning combination and created start new game

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './helpers';
 import "./styles/root.scss";
 
+const New_Game = [{ board: Array(9).fill(null), isXNext: true }];
+
 const App = () => {
 
-  const [ history, setHistory ] = useState([{ board: Array(9).fill(null), isXNext: true }]);
+  const [ history, setHistory ] = useState(New_Game);
 
   const [ currentMove, setCurrentMove ] = useState(0);
 
@@ -33,17 +35,23 @@ const App = () => {
       setCurrentMove(prev => prev + 1);
   }
 
-  const winner = calculateWinner(current.board);
+  const {winner, winningSquares } = calculateWinner(current.board);
 
   const moveTo = (move) => {
     setCurrentMove(move);
+  }
+
+  const onNewGame = () => {
+    setHistory(New_Game);
+    setCurrentMove(0);
   }
 
  return (
   <div className="app">
   <h1>TIC TAC TOE</h1>
   <StatusMessage winner={winner} current={current} />
-  <Board board={current.board} handleSquareClick={handleSquareClick} />
+  <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares} />
+  <button type="button" onClick={onNewGame}>Start New Game</button>
   <History history={history} moveTo={moveTo} currentMove={currentMove}/>
   </div>
  );
