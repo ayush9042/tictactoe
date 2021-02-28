423816f06165468e2629c54fd95c729a131d5a87
Ayush <ayushaggarwal9042@gmail.com>
2021-02-28 20:21:37 +0530

    styled the entire app

--- src/components/History.js
+++ src/components/History.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 const History = ({ history, moveTo, currentMove }) => {
   return (
-    <div>
-      <ul>
+    <div className="history-wrapper">
+      <ul className="history">
         {
           history.map((_, move) => {
             return(
               <li key={move}>
               <button
-                style={{
-                  fontWeight: move === currentMove ? 'bold' : 'normal',
-                }}
+                className={`btn-move ${move === currentMove ? 'active' : ''}`}
                type="button" onClick={() => {
                 moveTo(move)
               }}>{ move === 0 ? 'Go To Game Start' : `Go To Move #${move}`}</button>
--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -7,16 +7,22 @@ const StatusMessage = ({ winner, current }) => {
   const noMoveLeft = current.board.every((el) => el !== null);
 
   return (
-    <h2>
+    <div className="status-message">
       {
-        winner && `Winner is ${winner}`
+        winner && <>
+          Winner is { ' ' }<span className={winner === 'X' ? 'text-green' : 'text-orange'}>{winner}</span>
+        </>
       }{
-        !winner && !noMoveLeft && `Next Player is ${current.isXNext ? 'X' : 'O'}`
+        !winner && !noMoveLeft && <>
+          Next Player is <span className={current.isXNext ? 'text-green' : 'text-orange'}>{current.isXNext ? 'X' : 'O'}</span>
+        </> 
       }
       {
-        !winner && noMoveLeft && 'X And O Tied'
+        !winner && noMoveLeft && <>
+          <span className="text-green">X</span> and <span className="text-orange">O</span>
+        </>
       }
-    </h2>
+    </div>
   )
 }
 
